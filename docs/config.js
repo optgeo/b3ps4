bf3b2e84dfb4588f61f6103a515a86d9eb48574f
hfu <hfu@users.noreply.github.com>
2021-04-29 05:11:26 +0000

    update

--- docs/config.js
+++ docs/config.js
@@ -53,6 +53,58 @@ const config = {
         "bearing": 14.9,
         "pitch": 62.0
       }
+    },
+    {
+      "title": "阿蘇山",
+      "description": "阿蘇山は世界最大級のカルデラです。",
+      "hash": "10.82/32.9178/131.0562/109.3/63",
+      "id": "chapter-3",
+      "alignment": "right",
+      "callback": null,
+      "hidden": false,
+      "mapAnimation": "flyTo",
+      "rotateAnimation": true,
+      "onChapterEnter": [
+
+      ],
+      "onChapterExit": [
+
+      ],
+      "location": {
+        "zoom": 10.82,
+        "center": [
+          131.0562,
+          32.9178
+        ],
+        "bearing": 109.3,
+        "pitch": 63.0
+      }
+    },
+    {
+      "title": "大分",
+      "description": "石油化学や鉄鋼の生産が盛んです。",
+      "hash": "14.68/33.27065/131.67556/-116.9/80",
+      "id": "chapter-4",
+      "alignment": "right",
+      "callback": null,
+      "hidden": false,
+      "mapAnimation": "flyTo",
+      "rotateAnimation": true,
+      "onChapterEnter": [
+
+      ],
+      "onChapterExit": [
+
+      ],
+      "location": {
+        "zoom": 14.68,
+        "center": [
+          131.67556,
+          33.27065
+        ],
+        "bearing": -116.9,
+        "pitch": 80.0
+      }
     }
   ],
   "footer": "<p>YAML と GitHub Actions と <a href='https://optgeo.github.io/b3p/#15.7/35.680899/139.766841/0/49'>b3p</a>で <a href='https://github.com/mapbox/storytelling'>storytelling</a>しています。</p> <p>このプロジェクトは、 <a href='https://github.com/optgeo'>Adopt Geodata プロジェクト</a>の一環です。</p> <p>地理空間情報は人間の安全保障に貢献するデータですが、 人々のプライバシーや社会の安全に関わる悪用も可能な情報です。</p> <p>適切な<a href='https://ggim.un.org/'>情報管理</a>を行い、 <a href='https://www.mofa.go.jp/mofaj/files/100167362.pdf'>信頼を大切に</a>しながら <a href='https://www.cas.go.jp/jp/seisaku/sokuitiri/index.html'>地理空間情報の活用を推進</a> しましょう。</p>",
